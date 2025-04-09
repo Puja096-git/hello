45a1d549b97e79d64ab1919cfb48cbca3339f692
Puja Mahto <pujamahto@Pujas-MacBook-Air.local>
2025-04-10 01:08:34 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Add{
+    public static void main(String[] args) {
+        
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int a =  sc.next();
+        System.out.println(name);
+
+    }
+}
+
---
+++ Puja.java
@@ -0,0 +1,11 @@
+class Puja{
+    public static void main(String [] args){
+
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***\n****");
+    
+
+
+    }
+}
